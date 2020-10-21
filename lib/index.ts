44fa83cbc8a4d5986c6614abc8fadee55ad2a369
opl- <opl-@users.noreply.github.com>
2020-10-21 21:21:13 +0200

    Add more exports to the index

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,10 @@
-import {Router} from './Router';
+import {Middlewareable, MiddlewareLike, Router, RouterContext, RouterOptions, SpecialMethod} from './Router';
 
 export {
+	Middlewareable,
+	MiddlewareLike,
 	Router,
+	RouterContext,
+	RouterOptions,
+	SpecialMethod,
 };
